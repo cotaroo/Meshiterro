404f06ef4bb6a8c3cd528f85881ad87cd124ac22
cotaro.50tal@gmail.com <ec2-user@ip-172-31-90-118.ec2.internal>
2021-06-16 00:52:27 +0000

    [Finish] 5-3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '952717463eac49629d74ba70c5eb8d2467717a1afb8d3e1f8115d0b9f493909fe6ca908ac786a284d7e9f164031caadf50e027009104b3a12d50798d27c7c44e'
